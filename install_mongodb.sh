9fef427369cb02895ec485db9ea3f560a49bb99a
Belov Valeriy Andreevich <vbelov@sportmaster.ru>
2022-12-22 15:07:20 +0300

    HW-6. Fix scritps install mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,9 +4,12 @@ sudo apt-get install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
+sudo apt-get install --reinstall dbus
+sudo systemctl start dbus
 sudo apt-get install -y mongodb-org
-sleep 400
+sleep 60
 cat /lib/systemd/system/mongod.service
+sleep 60
 sudo systemctl start mongod
 sudo systemctl enable mongod
-sudo /usr/bin/mongod --config /etc/mongod.con
+sudo /usr/bin/mongod --config /etc/mongod.conf
