555d7910af6bce936b76cc705d380cb17290aeb1
Belov Valeriy Andreevich <vbelov@sportmaster.ru>
2023-02-01 11:31:30 +0300

    HW-12. ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -25,3 +25,19 @@ resource "yandex_compute_instance" "app" {
   }
 
 }
+data "yandex_vpc_network" "vpc" {
+  name = "default"
+}
+resource "yandex_vpc_security_group" "nsg" {
+  name        = "app security group"
+  description = "description for app security group"
+  network_id  = data.yandex_vpc_network.vpc.id
+}
+resource "yandex_vpc_security_group_rule" "port-80" {
+  security_group_binding = yandex_vpc_security_group.nsg.id
+  direction              = "ingress"
+  description            = "port-80"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  port                   = 80
+  protocol               = "TCP"
+}
